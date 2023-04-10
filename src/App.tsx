c94bf143e1621a734cc530521ad7120c13b89c4e
smartprincipal <johnsonoluwayemibunmi@gmail.com>
2023-04-10 13:01:36 -0700

    nav bar implemented

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+// import { useState } from 'react'
+
+import Navbar from "@/scenes/navbar"
+
+
+
+
+function App() {
+
+  return (
+    <div className="App bg-gray-200">
+      <Navbar/>
+    </div>
+  )
+}
+
+export default App
