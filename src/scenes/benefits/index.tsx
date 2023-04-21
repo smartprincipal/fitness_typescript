741da1ccb53cb2d426c26840ec71b322b3cd71e2
smartprincipal <johnsonoluwayemibunmi@gmail.com>
2023-04-21 16:28:22 -0700

    change effected

--- src/scenes/benefits/index.tsx
+++ src/scenes/benefits/index.tsx
@@ -20,7 +20,7 @@ const benefits: Array<BenefitType>= [
       "Neque adipiscing amet amet enim. Feugiat dolor enim fermentum in a in lectus pellentesque. Ullamcorper et.",
  },
  {
-  icon:<AcademicCapIcon className='h-6 w-6'/>,
+   icon:<AcademicCapIcon className='h-6 w-6'/>,
   title: "Expert and Pro Trainers",
   description:
       "Neque adipiscing amet amet enim. Feugiat dolor enim fermentum in a in lectus pellentesque. Ullamcorper et.",
@@ -35,7 +35,7 @@ const container ={
 }
 
 type Props = {
- setSelectedPage : (value: SelectedPage) => void
+ setSelectedPage : (value: SelectedPage) => void;
 }
 
 const Benefits = ({setSelectedPage}: Props) => {
@@ -51,17 +51,17 @@ const Benefits = ({setSelectedPage}: Props) => {
     variants={{
     hidden: {opacity:0 , x:-50},
     visible: {opacity:1, x:0},
-    }}
-   onViewportEnter={() =>setSelectedPage(SelectedPage.Benefits)}
-   >
-    <motion.div className='md:my-5 md:w-3/5'>
+  }}
+  onViewportEnter={() =>setSelectedPage(SelectedPage.Benefits)}
+  >
+
      <HText>MORE THAN JUST A GYM.</HText>
 
      <p className='my-5 text-sm'>
       We provide world class fitnes equipment, trainers and classes to get you to your ultimate fitness goal with ease. We provide true care into each and every member.
      </p>
-    </motion.div>
 
+     </motion.div>
 
     {/* BENEFITS SECTION */}
     <motion.div className='md:flex items-center justify-between gap-8 mt-5'
@@ -152,7 +152,6 @@ const Benefits = ({setSelectedPage}: Props) => {
     </div>
 
     </div>
-   </motion.div>
 
   </section>
 }
